6a13edeb3daa58adb8bd531eb6a5e7253557079b
Mykyta Herasymov <gerasimov08nikita@gmail.com>
2025-07-16 23:13:08 +0300

    Initial commit

---
+++ src/main/java/org/abrohamovich/littleshop/LittleshopApplication.java
@@ -0,0 +1,13 @@
+package org.abrohamovich.littleshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LittleshopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LittleshopApplication.class, args);
+    }
+
+}
