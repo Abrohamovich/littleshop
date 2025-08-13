1775e8e60037d8c6be2407dc5add7a1dbd09197d
Mykyta Herasymov <gerasimov08nikita@gmail.com>
2025-08-13 23:35:21 +0300

    fix: added creation and modification date field in category web response

--- src/main/java/org/abrohamovich/littleshop/adapter/web/category/CategoryWebResponse.java
+++ src/main/java/org/abrohamovich/littleshop/adapter/web/category/CategoryWebResponse.java
@@ -2,6 +2,8 @@ package org.abrohamovich.littleshop.adapter.web.category;
 
 import lombok.*;
 
+import java.time.LocalDateTime;
+
 @Getter
 @Setter
 @Builder
@@ -11,4 +13,6 @@ public class CategoryWebResponse {
     private Long id;
     private String name;
     private String description;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
 }
